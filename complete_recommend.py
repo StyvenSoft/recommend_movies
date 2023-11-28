7f17df62f5709e04de39d964333a7605ea74fb8b
Steveen Echeverri <steveen.echeverri@gmail.com>
2023-11-27 22:42:07 -0500

    Main prototype updated

--- complete_recommend.py
+++ complete_recommend.py
@@ -1,16 +1,40 @@
-import math
+from scipy.spatial.distance import cosine
 import json
 
 # Definir preguntas y géneros
 preguntas_generos = {
-    "Pregunta1": {"pregunta": "1. ¿Disfrutas de escenas de lucha y persecuciones emocionantes en las películas?", "genero": "Accion"},
-    "Pregunta2": {"pregunta": "2. ¿Te gustan las historias que involucran exploración y descubrimiento de lugares desconocidos?", "genero": "Aventura"},
-    "Pregunta3": {"pregunta": "3. ¿Te interesa la tecnología futurista y los conceptos científicos en las películas?", "genero": "Ciencia Ficcion"},
-    "Pregunta4": {"pregunta": "4. ¿Prefieres películas que te hagan reír y alivien el estrés?", "genero": "Comedia"},
-    "Pregunta5": {"pregunta": "5. ¿Te emocionan las historias profundas y emotivas sobre la vida y las relaciones?", "genero": "Drama"},
-    "Pregunta6": {"pregunta": "6. ¿Disfrutas de mundos imaginarios, criaturas mágicas y elementos fantásticos?", "genero": "Fantasia"},
-    "Pregunta7": {"pregunta": "7. ¿Te gusta la tensión y la intriga que se desarrolla a lo largo de una película?", "genero": "Suspenso"},
-    "Pregunta8": {"pregunta": "8. ¿Te emociona la sensación de miedo y los elementos sobrenaturales en las películas?", "genero": "Terror"}
+    "Pregunta1": {
+        "pregunta": "1. ¿Disfrutas de escenas de lucha y persecuciones emocionantes en las películas?",
+        "genero": "Accion"
+        },
+    "Pregunta2": {
+        "pregunta": "2. ¿Te gustan las historias que involucran exploración y descubrimiento de lugares desconocidos?",
+        "genero": "Aventura"
+        },
+    "Pregunta3": {
+        "pregunta": "3. ¿Te interesa la tecnología futurista y los conceptos científicos en las películas?", 
+        "genero": "Ciencia Ficcion"
+        },
+    "Pregunta4": {
+        "pregunta": "4. ¿Prefieres películas que te hagan reír y alivien el estrés?",
+        "genero": "Comedia"
+        },
+    "Pregunta5": {
+        "pregunta": "5. ¿Te emocionan las historias profundas y emotivas sobre la vida y las relaciones?",
+        "genero": "Drama"
+        },
+    "Pregunta6": {
+        "pregunta": "6. ¿Disfrutas de mundos imaginarios, criaturas mágicas y elementos fantásticos?",
+        "genero": "Fantasia"
+        },
+    "Pregunta7": {
+        "pregunta": "7. ¿Te gusta la tensión y la intriga que se desarrolla a lo largo de una película?",
+        "genero": "Suspenso"
+        },
+    "Pregunta8": {
+        "pregunta": "8. ¿Te emociona la sensación de miedo y los elementos sobrenaturales en las películas?",
+        "genero": "Terror"
+        }
 }
 
 with open('new_movies.json', 'r') as file:
@@ -52,10 +76,13 @@ respuestas_usuario = [value["genero"] for key, value in preguntas_generos.items(
 
 # Definir la función de similitud coseno entre dos vectores
 def cosine_similarity(vec1, vec2):
-    dot_product = sum(vec1[key] * vec2.get(key, 0) for key in vec1)
-    magnitude_vec1 = math.sqrt(sum(val ** 2 for val in vec1.values()))
-    magnitude_vec2 = math.sqrt(sum(val ** 2 for val in vec2.values()))
-    return dot_product / (magnitude_vec1 * magnitude_vec2) if magnitude_vec1 * magnitude_vec2 != 0 else 0
+    common_keys = set(vec1.keys()) & set(vec2.keys())
+    vec1_values = [vec1[key] for key in common_keys]
+    vec2_values = [vec2[key] for key in common_keys]
+
+    # Calcular la similitud del coseno usando scipy
+    similarity = 1 - cosine(vec1_values, vec2_values)
+    return similarity
 
 # Definir la función de filtrado colaborativo
 def collaborative_filtering(user_ratings, all_ratings):
@@ -82,21 +109,28 @@ def collaborative_filtering(user_ratings, all_ratings):
     # Devolver las películas recomendadas
     return [movie_id for movie_id, score in sorted_recommendations]
 
-# Inicializar el diccionario de puntuaciones del usuario
-user_ratings = {}
+# Función para crear el diccionario de puntuaciones del usuario
+def diccionario_puntuaciones(respuestas_usuario, movies):
+    # Inicializar el diccionario de puntuaciones del usuario
+    user_ratings = {}
+    
+    # Llenar el diccionario con las puntuaciones del usuario actual
+    for genre in respuestas_usuario:
+        for movie in movies:
+            if genre == movie["genero"]:
+                for user, rating in movie["puntuaciones"].items():
+                    user_ratings[user] = user_ratings.get(user, {})
+                    user_ratings[user][movie["titulo"]] = rating
 
-# Llenar el diccionario con las puntuaciones del usuario actual
-for genre in respuestas_usuario:
-    for movie in movies:
-        if genre == movie["genero"]:
-            for user, rating in movie["puntuaciones"].items():
-                user_ratings[user] = user_ratings.get(user, {})
-                user_ratings[user][movie["titulo"]] = rating
+    return user_ratings
+
+# Crear el diccionario de puntuaciones del usuario
+user_ratings = diccionario_puntuaciones(respuestas_usuario, movies)
 
 # Crear un diccionario con todas las puntuaciones de todos los usuarios
 all_ratings = {user: ratings for user, ratings in zip(user_ratings.keys(), user_ratings.values())}
 
-print(f"\nGenero de peliculas preferidos para : {nombre_usuario}!")
+print(f"\nGenero de peliculas preferidos para : {nombre_usuario}")
 
 print(respuestas_usuario)
 recomendaciones = collaborative_filtering(user_ratings["usuario1"], all_ratings)
